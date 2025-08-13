b5eba132d763263d4a606ae3ed7538eadbb583e2
hby1r25 <hby1r25@soton.ac.uk>
2025-08-13 15:20:09 +0100

    sphinx test

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# docs/conf.py
+
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+project = 'cornerstone-community'
+author = 'CORNERSTONE PDK team'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'myst_parser',
+]
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+
+#templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_book_theme'
+
+html_theme_options = {
+    "repository_url": "https://github.com/cornerstone-uos/cornerstone-community",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+}
+
+html_static_path = ['_static']
